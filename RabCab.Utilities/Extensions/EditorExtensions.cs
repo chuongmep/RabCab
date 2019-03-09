7149801cdbc79466062f16d08bf992d4a4402790
CraterSpace <zachayers.personal@icloud.com>
2019-03-09 13:02:39 -0600

    Prompt Distance Options added

--- RabCab.Utilities/Extensions/EditorExtensions.cs
+++ RabCab.Utilities/Extensions/EditorExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
 using RabCab.Utilities.Calculators;
 
 namespace RabCab.Utilities.Extensions
@@ -9,57 +10,335 @@ namespace RabCab.Utilities.Extensions
         #region Prompt Angle Options
 
         /// <summary>
-        /// Method to get an angle from the user
+        /// Method to get an angle from the user in radians
         /// </summary>
         /// <param name="acCurEd">The current working editor</param>
         /// <param name="prompt">The string to be prompted to the user</param>
-        /// <returns>Returns the value input by the user as a Radian</returns>
-        public static double GetRadian(this Editor acCurEd, string prompt)
-        {
+        /// <param name="defaultValue">The default value to use in prompt -> pressing enter will automatically use the default distance. Default value is input in Degrees</param>
+        /// <returns>Returns the value input by the user as a radian</returns>
+        public static double GetRadian(this Editor acCurEd, string prompt, double defaultValue = 0)
+        {//If default value, convert it from degrees to radians
+
+            if (defaultValue != 0)
+                defaultValue = UnitConverter.ConvertToRadians(defaultValue);
+
             //Prompt user to enter an angle in autoCAD
-            var prAngOpts = new PromptAngleOptions("")
+            var prAngOpts = new PromptAngleOptions(string.Empty)
             {
                 Message = prompt,
                 AllowNone = false,
-                AllowZero = false
+                AllowZero = false,
+                DefaultValue = defaultValue,
+                UseDefaultValue = defaultValue != 0,
             };
 
             //Prompt the editor to receive the angle from the user
             var prAngRes = acCurEd.GetAngle(prAngOpts);
 
-            //If bad input entered - return 0
+            //If bad input -> return 0
             if (prAngRes.Status != PromptStatus.OK) return 0;
 
-            //Get the angle entered from the editor
+            //Return the angle entered into the editor
             var doubleResult = prAngRes.Value;
             return doubleResult;
         }
 
         /// <summary>
-        /// Method to get an angle from the user
+        /// Method to get an angle from the user and return it in degrees
         /// </summary>
         /// <param name="acCurEd">The current working editor</param>
-        /// <param name="prompt">The string to be prompted to the user</param>
-        /// <returns>Returns the value input by the user as a Radian</returns>
-        public static double GetDegree(this Editor acCurEd, string prompt)
+        /// <param name="prompt">The prompt to present to the user</param>
+        /// <param name="defaultValue">The default value to use in prompt -> pressing enter will automatically use the default distance. Default value is input in degrees</param>
+        /// <returns>Returns the value input by the user as a degree</returns>
+        public static double GetDegree(this Editor acCurEd, string prompt, double defaultValue = 0)
         {
+            //If default value, convert it from degrees to radians
+            if (defaultValue != 0)
+                defaultValue = UnitConverter.ConvertToRadians(defaultValue);
+
             //Prompt user to enter an angle in autoCAD
-            var prAngOpts = new PromptAngleOptions("")
+            var prAngOpts = new PromptAngleOptions(string.Empty)
             {
                 Message = prompt,
                 AllowNone = false,
-                AllowZero = false
+                AllowZero = false,
+                DefaultValue = defaultValue,
+                UseDefaultValue = defaultValue != 0,
             };
 
             //Prompt the editor to receive the angle from the user
             var prAngRes = acCurEd.GetAngle(prAngOpts);
 
-            //If bad input entered - return 0
+            //If bad input -> return 0
             if (prAngRes.Status != PromptStatus.OK) return 0;
 
-            //Get the angle entered from the editor
+            //Return the angle entered into the editor
             var doubleResult = prAngRes.Value;
             return UnitConverter.ConvertToDegrees(doubleResult);
+        }
+
+        #endregion
+
+        #region Prompt Distance Options
+
+        /// <summary>
+        /// Gets any 3D distance in CAD, allows positive and negative values.
+        /// </summary>
+        /// <param name="acCurEd">The current working Editor.</param>
+        /// <param name="prompt">The prompt to present to the user.</param>
+        /// <param name="defaultValue">The default value to use in prompt -> pressing enter will automatically use the default distance.</param>
+        /// <returns>Returns a distance from the editor in decimal format.</returns>
+        public static double GetAnyDistance(this Editor acCurEd, string prompt, double defaultValue = 0)
+        {
+            //Prompt user to enter a distance in autoCAD
+            var prDistOpts = new PromptDistanceOptions(string.Empty)
+            {
+                Message = prompt,
+                AllowNone = false,
+                AllowZero = true,
+                DefaultValue = defaultValue,
+                UseDefaultValue = defaultValue != 0,
+            };
+
+            //Prompt the editor to receive the distance from the user
+            var prDistRes = acCurEd.GetDistance(prDistOpts);
+
+            //If bad input -> return 0
+            if (prDistRes.Status != PromptStatus.OK) return 0;
+
+            //Return the distance entered into the editor
+            var distResult = prDistRes.Value;
+            return distResult;
+        }
+
+        /// <summary>
+        /// Gets any 3D distance in CAD, uses a base point and allows positive and negative values.
+        /// </summary>
+        /// <param name="acCurEd">The current working Editor.</param>
+        /// <param name="basePt">The point to start the distance from.</param>
+        /// <param name="prompt">The prompt to present to the user.</param>
+        /// <param name="defaultValue">The default value to use in prompt -> pressing enter will automatically use the default distance.</param>
+        /// <returns>Returns a distance from the editor in decimal format.</returns>
+        public static double GetAnyDistance(this Editor acCurEd, Point3d basePt, string prompt, double defaultValue = 0)
+        {
+            //Prompt user to enter a distance in autoCAD
+            var prDistOpts = new PromptDistanceOptions(string.Empty)
+            {
+                Message = prompt,
+                AllowNone = false,
+                AllowZero = true,
+                BasePoint = basePt,
+                UseBasePoint = true,
+                DefaultValue = defaultValue,
+                UseDefaultValue = defaultValue != 0,
+            };
+
+            //Prompt the editor to receive the distance from the user
+            var prDistRes = acCurEd.GetDistance(prDistOpts);
+
+            //If bad input -> return 0
+            if (prDistRes.Status != PromptStatus.OK) return 0;
+
+            //Return the distance entered into the editor
+            var distResult = prDistRes.Value;
+            return distResult;
+        }
+
+        /// <summary>
+        /// Gets any 3D distance in CAD, allows positive values.
+        /// </summary>
+        /// <param name="acCurEd">The current working Editor.</param>
+        /// <param name="prompt">The prompt to present to the user.</param>
+        /// <param name="defaultValue">The default value to use in prompt -> pressing enter will automatically use the default distance.</param>
+        /// <returns>Returns a distance from the editor in decimal format.</returns>
+        public static double GetPositiveDistance(this Editor acCurEd, string prompt, double defaultValue = 0)
+        {
+            //Prompt user to enter a distance in autoCAD
+            var prDistOpts = new PromptDistanceOptions(string.Empty)
+            {
+                Message = prompt,
+                AllowNone = false,
+                AllowZero = true,
+                AllowNegative = false,
+                DefaultValue = defaultValue,
+                UseDefaultValue = defaultValue != 0,
+            };
+
+            //Prompt the editor to receive the distance from the user
+            var prDistRes = acCurEd.GetDistance(prDistOpts);
+
+            //If bad input -> return 0
+            if (prDistRes.Status != PromptStatus.OK) return 0;
+
+            //Return the distance entered into the editor
+            var distResult = prDistRes.Value;
+            return distResult;
+        }
+
+        /// <summary>
+        /// Gets any 3D distance in CAD, uses a base point and allows positive values.
+        /// </summary>
+        /// <param name="acCurEd">The current working Editor.</param>
+        /// <param name="basePt">The point to start the distance from.</param>
+        /// <param name="prompt">The prompt to present to the user.</param>
+        /// <param name="defaultValue">The default value to use in prompt -> pressing enter will automatically use the default distance.</param>
+        /// <returns>Returns a distance from the editor in decimal format.</returns>
+        public static double GetPositiveDistance(this Editor acCurEd, Point3d basePt, string prompt, double defaultValue = 0)
+        {
+            //Prompt user to enter a distance in autoCAD
+            var prDistOpts = new PromptDistanceOptions(string.Empty)
+            {
+                Message = prompt,
+                AllowNone = false,
+                AllowZero = true,
+                AllowNegative = false,
+                BasePoint = basePt,
+                UseBasePoint = true,
+                DefaultValue = defaultValue,
+                UseDefaultValue = defaultValue != 0,
+            };
+
+            //Prompt the editor to receive the distance from the user
+            var prDistRes = acCurEd.GetDistance(prDistOpts);
+
+            //If bad input -> return 0
+            if (prDistRes.Status != PromptStatus.OK) return 0;
+
+            //Return the distance entered into the editor
+            var distResult = prDistRes.Value;
+            return distResult;
+        }
+
+        /// <summary>
+        /// Gets any 2D distance in CAD, allows positive and negative values.
+        /// </summary>
+        /// <param name="acCurEd">The current working Editor.</param>
+        /// <param name="prompt">The prompt to present to the user.</param>
+        /// /// <param name="defaultValue">The default value to use in prompt -> pressing enter will automatically use the default distance.</param>
+        /// <returns>SReturns a distance from the editor in decimal format.</returns>
+        public static double GetAny2DDistance(this Editor acCurEd, string prompt, double defaultValue = 0)
+        {
+            //Prompt user to enter a distance in autoCAD
+            var prDistOpts = new PromptDistanceOptions(string.Empty)
+            {
+                Message = prompt,
+                AllowNone = false,
+                AllowZero = true,
+                Only2d = true,
+                DefaultValue = defaultValue,
+                UseDefaultValue = defaultValue != 0,
+            };
+
+            //Prompt the editor to receive the distance from the user
+            var prDistRes = acCurEd.GetDistance(prDistOpts);
+
+            //If bad input -> return 0
+            if (prDistRes.Status != PromptStatus.OK) return 0;
+
+            //Return the distance entered into the editor
+            var distResult = prDistRes.Value;
+            return distResult;
+        }
+
+        /// <summary>
+        /// Gets any 2D distance in CAD, uses a base point and allows positive and negative values.
+        /// </summary>
+        /// <param name="acCurEd">The current working Editor.</param>
+        /// <param name="basePt">The point to start the distance from.</param>
+        /// <param name="prompt">The prompt to present to the user.</param>
+        /// <param name="defaultValue">The default value to use in prompt -> pressing enter will automatically use the default distance.</param>
+        /// <returns>Returns a distance from the editor in decimal format.</returns>
+        public static double GetAny2DDistance(this Editor acCurEd, Point3d basePt, string prompt, double defaultValue = 0)
+        {
+            //Prompt user to enter a distance in autoCAD
+            var prDistOpts = new PromptDistanceOptions(string.Empty)
+            {
+                Message = prompt,
+                AllowNone = false,
+                AllowZero = true,
+                Only2d = true,
+                BasePoint = basePt,
+                UseBasePoint = true,
+                DefaultValue = defaultValue,
+                UseDefaultValue = defaultValue != 0,
+            };
+
+            //Prompt the editor to receive the distance from the user
+            var prDistRes = acCurEd.GetDistance(prDistOpts);
+
+            //If bad input -> return 0
+            if (prDistRes.Status != PromptStatus.OK) return 0;
+
+            //Return the distance entered into the editor
+            var distResult = prDistRes.Value;
+            return distResult;
+        }
+
+        /// <summary>
+        /// Gets any 2D distance in CAD, allows positive values.
+        /// </summary>
+        /// <param name="acCurEd">The current working Editor.</param>
+        /// <param name="prompt">The prompt to present to the user.</param>
+        /// <param name="defaultValue">The default value to use in prompt -> pressing enter will automatically use the default distance.</param>
+        /// <returns>Returns a distance from the editor in decimal format.</returns>
+        public static double GetPositive2DDistance(this Editor acCurEd, string prompt, double defaultValue = 0)
+        {
+            //Prompt user to enter a distance in autoCAD
+            var prDistOpts = new PromptDistanceOptions(string.Empty)
+            {
+                Message = prompt,
+                AllowNone = false,
+                AllowZero = true,
+                Only2d = true,
+                AllowNegative = false,
+                DefaultValue = defaultValue,
+                UseDefaultValue = defaultValue != 0,
+            };
+
+            //Prompt the editor to receive the distance from the user
+            var prDistRes = acCurEd.GetDistance(prDistOpts);
+
+            //If bad input -> return 0
+            if (prDistRes.Status != PromptStatus.OK) return 0;
+
+            //Return the distance entered into the editor
+            var distResult = prDistRes.Value;
+            return distResult;
+        }
+
+        /// <summary>
+        /// Gets any 2D distance in CAD, allows positive values.
+        /// </summary>
+        /// <param name="acCurEd">The current working Editor.</param>
+        /// <param name="basePt">The point to start the distance from.</param>
+        /// <param name="prompt">The prompt to present to the user.</param>
+        /// <param name="defaultValue">The default value to use in prompt -> pressing enter will automatically use the default distance.</param>
+        /// <returns>Returns a distance from the editor in decimal format.</returns>
+        public static double GetPositive2DDistance(this Editor acCurEd, Point3d basePt, string prompt, double defaultValue = 0)
+        {
+            //Prompt user to enter a distance in autoCAD
+            var prDistOpts = new PromptDistanceOptions(string.Empty)
+            {
+                Message = prompt,
+                AllowNone = false,
+                AllowZero = true,
+                Only2d = true,
+                AllowNegative = false,
+                BasePoint = basePt,
+                UseBasePoint = true,
+                DefaultValue = defaultValue,
+                UseDefaultValue = defaultValue != 0
+            };
+
+            //Prompt the editor to receive the distance from the user
+            var prDistRes = acCurEd.GetDistance(prDistOpts);
+
+            //If bad input -> return 0
+            if (prDistRes.Status != PromptStatus.OK) return 0;
+
+            //Return the distance entered into the editor
+            var distResult = prDistRes.Value;
+            return distResult;
         }
 
         #endregion
--- RabCab.Utilities/Initialization/myCommands.cs
+++ RabCab.Utilities/Initialization/myCommands.cs
@@ -1,20 +1,21 @@
 ﻿// (C) Copyright 2019 by  
 //
 
-using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.ApplicationServices.Core;
 using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
 using Autodesk.AutoCAD.Runtime;
 using RabCab.Utilities.Extensions;
 using RabCab.Utilities.Initialization;
 using static RabCab.Utilities.Settings.SettingsInternal;
 
 // This line is not mandatory, but improves loading performances
-[assembly: CommandClass(typeof(MyCommands))]
+
+[assembly: CommandClass(typeof (MyCommands))]
 
 namespace RabCab.Utilities.Initialization
 {
-
     // This class is instantiated by AutoCAD for each document when
     // a command is called by the user the first time in the context
     // of a given document. In other words, non static data in this class
@@ -23,34 +24,100 @@ namespace RabCab.Utilities.Initialization
     {
         #region Internal & Debug Commands
 
-        [CommandMethod(CommandGroup, "_CheckEditor", CommandFlags.Modal)]
+        [CommandMethod(CommandGroup, "Debug_CheckPrompts", CommandFlags.Modal)]
         public void DebugListCmdName() // This method can have any name
         {
-            var acCurDoc = Autodesk.AutoCAD.ApplicationServices.Core.Application.DocumentManager.MdiActiveDocument;
+            var acCurDoc = Application.DocumentManager.MdiActiveDocument;
             var acCurDb = acCurDoc.Database;
             var acCurEd = acCurDoc.Editor;
 
+            #region Prompt Angle Testing
+
+            //Angle Prompt Testing
             var angleRadTest = acCurEd.GetRadian("\nEnter angle to test prompt: ");
             acCurEd.WriteMessage("\n" + angleRadTest);
 
             var angleDegTest = acCurEd.GetDegree("\nEnter angle to test prompt: ");
             acCurEd.WriteMessage("\n" + angleDegTest);
+
+            //Angle Prompt Testing with Default Value
+            var angleRadTest2 = acCurEd.GetRadian("\nEnter angle to test prompt: ", 90);
+            acCurEd.WriteMessage("\n" + angleRadTest2);
+
+            var angleDegTest2 = acCurEd.GetDegree("\nEnter angle to test prompt: ", 90);
+            acCurEd.WriteMessage("\n" + angleDegTest2);
+
+            #endregion
+
+            #region Prompt Distance Testing
+
+            //3D Distance Prompt Testing
+            var distTest1 = acCurEd.GetAnyDistance("\nEnter any 3D distance: ");
+            acCurEd.WriteMessage("\n" + distTest1);
+
+            var distTest2 = acCurEd.GetAnyDistance(Point3d.Origin, "\nEnter any 3D distance: ");
+            acCurEd.WriteMessage("\n" + distTest2);
+
+            var distTest3 = acCurEd.GetPositiveDistance("\nEnter any positive 3D distance: ");
+            acCurEd.WriteMessage("\n" + distTest3);
+
+            var distTest4 = acCurEd.GetPositiveDistance(Point3d.Origin, "\nEnter any positive 3D distance: ");
+            acCurEd.WriteMessage("\n" + distTest4);
+
+            //2D Distance Prompt Testing
+            var distTest5 = acCurEd.GetAny2DDistance("\nEnter any 2D distance: ");
+            acCurEd.WriteMessage("\n" + distTest5);
+
+            var distTest6 = acCurEd.GetAny2DDistance(Point3d.Origin, "\nEnter any 2D distance: ");
+            acCurEd.WriteMessage("\n" + distTest6);
+
+            var distTest7 = acCurEd.GetPositive2DDistance("\nEnter any positive 2D distance: ");
+            acCurEd.WriteMessage("\n" + distTest7);
+
+            var distTest8 = acCurEd.GetPositive2DDistance(Point3d.Origin, "\nEnter any positive 2D distance: ");
+            acCurEd.WriteMessage("\n" + distTest8);
+
+            //3D Distance Prompt Testing with Default Value
+            var distTest9 = acCurEd.GetAnyDistance("\nEnter any 3D distance: ", 1);
+            acCurEd.WriteMessage("\n" + distTest9);
+
+            var distTest10 = acCurEd.GetAnyDistance(Point3d.Origin, "\nEnter any 3D distance: ", 1);
+            acCurEd.WriteMessage("\n" + distTest10);
+
+            var distTest11 = acCurEd.GetPositiveDistance("\nEnter any positive 3D distance: ", 1);
+            acCurEd.WriteMessage("\n" + distTest11);
+
+            var distTest12 = acCurEd.GetPositiveDistance(Point3d.Origin, "\nEnter any positive 3D distance: ", 1);
+            acCurEd.WriteMessage("\n" + distTest12);
+
+            //2D Distance Prompt Testing with Default Value
+            var distTest13 = acCurEd.GetAny2DDistance("\nEnter any 2D distance: ", 1);
+            acCurEd.WriteMessage("\n" + distTest13);
+
+            var distTest14 = acCurEd.GetAny2DDistance(Point3d.Origin, "\nEnter any 2D distance: ", 1);
+            acCurEd.WriteMessage("\n" + distTest14);
+
+            var distTest15 = acCurEd.GetPositive2DDistance("\nEnter any positive 2D distance: ", 1);
+            acCurEd.WriteMessage("\n" + distTest15);
+
+            var distTest16 = acCurEd.GetPositive2DDistance(Point3d.Origin, "\nEnter any positive 2D distance: ", 1);
+            acCurEd.WriteMessage("\n" + distTest16);
+
+            #endregion
         }
 
-
         #endregion
-
 
         #region Templates
 
         // The CommandMethod attribute can be applied to any public  member 
         // function of any public class.
         // The function should take no arguments and return nothing.
-        // If the method is an intance member then the enclosing class is 
-        // intantiated for each document. If the member is a static member then
-        // the enclosing class is NOT intantiated.
+        // If the method is an instance member then the enclosing class is 
+        // instantiated for each document. If the member is a static member then
+        // the enclosing class is NOT instantiated.
         //
-        // NOTE: CommandMethod has overloads where you can provide helpid and
+        // NOTE: CommandMethod has overloads where you can provide helpId and
         // context menu.
 
         // Modal Command with localized name
@@ -58,30 +125,22 @@ namespace RabCab.Utilities.Initialization
         public void MyCommand() // This method can have any name
         {
             // Put your command code here
-            Document doc = Application.DocumentManager.MdiActiveDocument;
-            Editor ed;
-            if (doc != null)
-            {
-                ed = doc.Editor;
-                ed.WriteMessage("Hello, this is your first command.");
+            var doc = Application.DocumentManager.MdiActiveDocument;
+            if (doc == null) return;
 
-            }
+            var ed = doc.Editor;
+            ed.WriteMessage("Hello, this is your first command.");
         }
 
-        // Modal Command with pickfirst selection
+        // Modal Command with pickFirst selection
         [CommandMethod("MyGroup", "MyPickFirst", "MyPickFirstLocal", CommandFlags.Modal | CommandFlags.UsePickSet)]
         public void MyPickFirst() // This method can have any name
         {
-            PromptSelectionResult result = Application.DocumentManager.MdiActiveDocument.Editor.GetSelection();
+            var result = Application.DocumentManager.MdiActiveDocument.Editor.GetSelection();
             if (result.Status == PromptStatus.OK)
             {
                 // There are selected entities
-                // Put your command using pickfirst set code here
-            }
-            else
-            {
-                // There are no selected entities
-                // Put your command code here
+                // Put your command using pickFirst set code here
             }
         }
 
@@ -105,7 +164,5 @@ namespace RabCab.Utilities.Initialization
         }
 
         #endregion
-
     }
-
 }
