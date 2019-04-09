201a899434b2dd14502f24b6f70cf86365321f18
CraterSpace <zjayers.social@gmail.com>
2019-04-09 15:50:48 -0500

    Updated Class Names

--- RabCab.Utilities/Initialization/InitPlugin.cs
+++ RabCab.Utilities/Initialization/InitPlugin.cs
@@ -5,14 +5,14 @@ using Autodesk.AutoCAD.Runtime;
 using RabCab.Utilities.Initialization;
 
 // This line is not mandatory, but improves loading performances
-[assembly: ExtensionApplication(typeof(MyPlugin))]
+[assembly: ExtensionApplication(typeof(InitPlugin))]
 
 namespace RabCab.Utilities.Initialization
 {
     // This class is instantiated by AutoCAD once and kept alive for the 
     // duration of the session. If you don't do any one time initialization 
     // then you should remove this class.
-    public class MyPlugin : IExtensionApplication
+    public class InitPlugin : IExtensionApplication
     {
         void IExtensionApplication.Initialize()
         {
