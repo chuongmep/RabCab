aa338ac73b0faff05a2b34ce504224c10580929f
CraterSpace <zjayers.social@gmail.com>
2019-04-10 16:34:03 -0500

    Debugger working

--- DebugConsole/DebugSandbox.cs
+++ DebugConsole/DebugSandbox.cs
@@ -3,18 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using RabCab.Utilities;
-using RabCab.Utilities.Initialization;
+using RabCab;
+using RabCab.Initialization;
 
 namespace DebugConsole
 {
-    class DebugSandbox
+    static class DebugSandbox
     {
+        /// <summary>
+        /// Main method
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            
-            Debugging.Cmd_TestXml();
-            Console.ReadLine();
+            Console.WriteLine("-------- RabCab Debugger --------\n");
+            Console.WriteLine("Please enter commands to test...");
+            ListenForInput();
+        }
+
+        /// <summary>
+        /// Method for recursively listening for input from debugger
+        /// </summary>
+        private static void ListenForInput()
+        {
+            //Set listening variable
+            var listening = true;
+
+            //While user does not choose to exit, keep asking for input
+            while (listening)
+            {
+                //Write an empty line to space out commands
+                Console.WriteLine("");
+
+                //Read input from the user
+                var input = Console.ReadLine();
+
+                switch (input?.ToUpper())
+                {
+                    case "XMLREAD":
+                        RabCab.Initialization.Debugging.Cmd_TestXml();
+                        break;
+
+                    case "EXIT":
+                        listening = false;
+                        break;
+                    default:
+                        Console.Write("No command exists with that name.");
+                        break;
+                }
+            }
         }
     }
 }
