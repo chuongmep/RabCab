194e5bc24757c2bb5420a071b3e3cee0df502e65
CraterSpace <zjayers.social@gmail.com>
2019-05-09 12:22:47 -0500

    cmt

--- RabCab.Utilities/Commands/PaletteKit/RcPaletteNotebook.cs
+++ RabCab.Utilities/Commands/PaletteKit/RcPaletteNotebook.cs
@@ -9,49 +9,43 @@
 //     References:          
 // -----------------------------------------------------------------------------------
 
-using System;
-using System.Windows.Forms;
 using Autodesk.AutoCAD.Runtime;
-using Autodesk.AutoCAD.Windows;
-using RabCab.Agents;
 using RabCab.Settings;
+using System;
 using Application = Autodesk.AutoCAD.ApplicationServices.Core.Application;
 
 namespace RabCab.Commands.PaletteKit
 {
     internal class RcPaletteNotebook
     {
-        private PaletteSet RcPal;
-        private UserControl PalPanel;
-        private string PalName = "Notebook";
 
         /// <summary>
         /// </summary>
         [CommandMethod(SettingsInternal.CommandGroup, "_RCNOTEPAL",
             CommandFlags.Modal
-            //| CommandFlags.Transparent
-            //| CommandFlags.UsePickSet
-            //| CommandFlags.Redraw
-            //| CommandFlags.NoPerspective
-            //| CommandFlags.NoMultiple
-            //| CommandFlags.NoTileMode
-            //| CommandFlags.NoPaperSpace
-            //| CommandFlags.NoOem
-            //| CommandFlags.Undefined
-            //| CommandFlags.InProgress
-            //| CommandFlags.Defun
-            //| CommandFlags.NoNewStack
-            //| CommandFlags.NoInternalLock
-            //| CommandFlags.DocReadLock
-            //| CommandFlags.DocExclusiveLock
-            //| CommandFlags.Session
-            //| CommandFlags.Interruptible
-            //| CommandFlags.NoHistory
-            //| CommandFlags.NoUndoMarker
-            //| CommandFlags.NoBlockEditor
-            //| CommandFlags.NoActionRecording
-            //| CommandFlags.ActionMacro
-            //| CommandFlags.NoInferConstraint 
+        //| CommandFlags.Transparent
+        //| CommandFlags.UsePickSet
+        //| CommandFlags.Redraw
+        //| CommandFlags.NoPerspective
+        //| CommandFlags.NoMultiple
+        //| CommandFlags.NoTileMode
+        //| CommandFlags.NoPaperSpace
+        //| CommandFlags.NoOem
+        //| CommandFlags.Undefined
+        //| CommandFlags.InProgress
+        //| CommandFlags.Defun
+        //| CommandFlags.NoNewStack
+        //| CommandFlags.NoInternalLock
+        //| CommandFlags.DocReadLock
+        //| CommandFlags.DocExclusiveLock
+        //| CommandFlags.Session
+        //| CommandFlags.Interruptible
+        //| CommandFlags.NoHistory
+        //| CommandFlags.NoUndoMarker
+        //| CommandFlags.NoBlockEditor
+        //| CommandFlags.NoActionRecording
+        //| CommandFlags.ActionMacro
+        //| CommandFlags.NoInferConstraint 
         )]
         public void Cmd_RcNotePal()
         {
@@ -60,41 +54,5 @@ namespace RabCab.Commands.PaletteKit
             var acCurDb = acCurDoc.Database;
             var acCurEd = acCurDoc.Editor;
         }
-
-        #region Pal Initialization
-
-        /// <summary>
-        /// TODO
-        /// </summary>
-        private void CreatePal()
-        {
-            if (RcPal == null)
-            {
-                RcPal = new PaletteSet(PalName, new Guid())
-                {
-                    Style = PaletteSetStyles.ShowPropertiesMenu
-                            | PaletteSetStyles.ShowAutoHideButton
-                            | PaletteSetStyles.ShowCloseButton
-                };
-
-                PalPanel = new UserControl();
-
-                PopulatePal();
-                PalPanel.UpdateTheme();
-                RcPal.Add(PalName, PalPanel);
-            }
-
-            RcPal.Visible = true;
-        }
-
-        /// <summary>
-        /// TODO
-        /// </summary>
-        private void PopulatePal()
-        {
-
-        }
-
-        #endregion
     }
 }
