5602e6cad7745ebb61a3eb1b16d34955f8b81282
CraterSpace <zjayers.social@gmail.com>
2019-06-03 08:41:13 -0500

    Scatter Done

--- RabCab.Utilities/Commands/AssemblySuite/RcScatter.cs
+++ RabCab.Utilities/Commands/AssemblySuite/RcScatter.cs
@@ -9,8 +9,15 @@
 //     References:          
 // -----------------------------------------------------------------------------------
 
+using System;
 using Autodesk.AutoCAD.ApplicationServices.Core;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
 using Autodesk.AutoCAD.Runtime;
+using RabCab.Agents;
+using RabCab.Analysis;
+using RabCab.Engine.Enumerators;
+using RabCab.Extensions;
 using RabCab.Settings;
 
 namespace RabCab.Commands.AssemblySuite
@@ -19,7 +26,7 @@ namespace RabCab.Commands.AssemblySuite
     {
         /// <summary>
         /// </summary>
-        [CommandMethod(SettingsInternal.CommandGroup, "_CMDDEFAULT",
+        [CommandMethod(SettingsInternal.CommandGroup, "_SCATTER",
             CommandFlags.Modal
             //| CommandFlags.Transparent
             //| CommandFlags.UsePickSet
@@ -45,12 +52,38 @@ namespace RabCab.Commands.AssemblySuite
             //| CommandFlags.ActionMacro
             //| CommandFlags.NoInferConstraint 
         )]
-        public void Cmd_Default()
+        public void Cmd_Scatter()
         {
             //Get the current document utilities
             var acCurDoc = Application.DocumentManager.MdiActiveDocument;
             var acCurDb = acCurDoc.Database;
             var acCurEd = acCurDoc.Editor;
+
+            var objIds = acCurEd.GetAllSelection(false);
+            if (objIds.Length <= 0) return;
+
+            var random = new Random();
+            
+            using (var acTrans = acCurDb.TransactionManager.StartTransaction())
+            {
+                var extents = acTrans.GetExtents(objIds, acCurDb);
+                var extCen = Solid3DExtensions.GetBoxCenter(extents.MinPoint, extents.MaxPoint);
+
+                foreach (var obj in objIds)
+                {
+                    var acEnt = acTrans.GetObject(obj, OpenMode.ForWrite) as Entity;
+                    if (acEnt == null) continue;
+
+                    var entExt = acTrans.GetExtents(new []{acEnt.ObjectId}, acCurDb);
+                    var entCen = Solid3DExtensions.GetBoxCenter(entExt.MinPoint, entExt.MaxPoint);
+                    
+                    var cPower = extCen.GetVectorTo(entCen).MultiplyBy(random.NextDouble());
+
+                    acEnt.TransformBy(Matrix3d.Displacement(cPower));
+                }
+
+                acTrans.Commit();
+            }
         }
     }
 }
